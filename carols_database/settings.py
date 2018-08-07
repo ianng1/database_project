73a3af6ac281220b3381928e2710a899df0ee90a
Peng Wah Ng <pengwah@pengs-imac.attlocal.net>
2018-08-07 14:40:26 -0700

    students app

--- carols_database/settings.py
+++ carols_database/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'carols_database.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
