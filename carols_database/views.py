c7ed540a1bbcf61ee4d61da8cdba085a86e8031f
Peng Wah Ng <pengwah@pengs-imac.attlocal.net>
2018-08-11 14:35:13 -0700

    finally got the database to work yay

--- carols_database/views.py
+++ carols_database/views.py
@@ -1,24 +1,2 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html')
-
-def test(request):
-    return render(request, 'test.html', {'key': 'yet another test'})
-
-def finalcount(request):
-    original_text = request.GET['fulltext']
-    split_text = original_text.split()
-    word_count = len(split_text)
-    wordDict = {}
-    for word in split_text:
-        if word in wordDict:
-            wordDict[word] += 1
-        else:
-            wordDict[word] = 1
-
-    return render(request, 'finalcount.html', {'original_text':original_text, 'word_count': word_count, 'wordDict': wordDict.items()})
-
-def troll(request):
-    return render(request, 'troll.html')
--- students/views.py
+++ students/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
+
+
+
 
 # Create your views here.
+def create(request):
+    if request.method == 'POST':
+        if request.POST['first name'] and request.POST['last name'] and request.POST['birthday'] and request.POST['email']:
+            student = Student()
+            student.first_name = request.POST['first name']
+            student.last_name = request.POST['last name']
+            student.birthday = request.POST['birthday']
+            student.email = request.POST['email']
+            student.save()
+            print("reached")
+            return redirect('create')
+        else:
+            return render(request, 'students/create.html', {'error': 'Did not fill in all fields'})
+    else:
+        return render(request, 'students/create.html')
