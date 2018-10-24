8fa3a9da671fe664e15ae76307b993be051a3044
Peng Wah Ng <pengwah@pengs-imac.attlocal.net>
2018-10-23 20:31:51 -0700

    email

--- carols_database/settings.py
+++ carols_database/settings.py
@@ -27,7 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'Admin <admin@pacificinstituteofmusic.tk>'
 # Application definition
 
 INSTALLED_APPS = [
